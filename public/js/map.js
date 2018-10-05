fc78dc0b51ef04721408463b7923f652e8529266
CaritoDasilva <caritodasilva@gmail.com>
2018-10-05 00:43:56 -0300

    se suben los cambios 0:43

--- public/js/map.js
+++ public/js/map.js
@@ -114,7 +114,103 @@ function updatePosition(event) {
   map.addObject(marker);
   map.setCenter(HEREHQcoordinates);
 }
+// Se mockean marcadores
 
+coffeeMarkers = [{
+    nombre: "Café Bacán", //Pedro de Valdivia 7023
+    lat: -33.4886194,
+    lng: -70.6063449
+  },
+  {
+    nombre: "Café Choris", //Pedro de Valdivia 5023
+    lat: -33.4756613,
+    lng: -70.6063725,
+  },
+  {
+    nombre: "Café del Panita",
+    lat: -33.4746844,
+    lng: -70.6064394
+  },
+
+  { //Providencia
+    nombre: "Café Guayoyo",
+    lat: -33.43638,
+    lng: -70.61488
+  },
+  {
+    nombre: "Papelón con limón",
+    lat: -33.4342108,
+    lng: -70.6151838
+  },
+
+  {
+    nombre: "Cefetería Al tiro",
+    lat: -33.4342108,
+    lng: -70.6151838
+  },
+  { //Recoleta
+    nombre: "Cefetería del Pato",
+    lat: -33.41913,
+    lng: -70.64185
+  },
+
+  {
+    nombre: "Las sabrosonas",
+    lat: -30.48875,
+    lng: -71.15639
+  },
+  {
+    nombre: "Aki se come Yico",
+    lat: -33.4191791,
+    lng: -70.6416367
+  },
+  { //Talagante
+    nombre: "Tengo de Toddy",
+    lat: -33.66664,
+    lng: -70.91623
+  },
+
+  {
+    nombre: "Come y Calla",
+    lat: -33.6671447,
+    lng: -70.9170176
+  },
+
+  {
+    nombre: "Cafetería Ando con Hambre",
+    lat: -33.74562,
+    lng: -70.91204
+  },
+
+]
+
+var coffeeIconUrl = '../img/Group 2.1.png';
+
+var coffeeIconOptions = {
+  // The icon's size in pixel:
+  size: new H.math.Size(26, 34),
+  // The anchorage point in pixel, 
+  // defaults to bottom-center
+  anchor: new H.math.Point(14, 34)
+};
+
+// Características del ícono del marcador
+
+var coffeeMarkerOptions = {
+  icon: new H.map.Icon(coffeeIconUrl, coffeeIconOptions)
+};
+
+function addingCofeeMarkers() {
+  coffeeMarkers.forEach(coffee => {
+
+    console.log(coffee);
+    var marker = new H.map.Marker(coffe, markerOptions);
+    map.addObject(marker);
+    map.removeObjects(markers); // remueve marcadores cuando cambias de geolocalización
+    markers = []; // almacena los marcadores
+  });
+
+}
 // Esta línea que va a continuación detona la función de la geolocalización con el cargador
 navigator.geolocation.watchPosition(updatePosition);
 
